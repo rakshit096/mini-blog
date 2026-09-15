10b6bcfdb086cbdc3479e5846405aeb8565428b4
rakshit096 <rakshitroyalbrother@gmail.com>
2026-09-15 17:00:14 +0530

    feat: add sessions controller code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,19 @@
 class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        user=User.find_by(email: params[:email])
+
+        if user && user.authenticate(params[:email])
+            session[:user_id]=user.id
+
+            redirect_to articles_path
+        else
+            flash.now[:alert]= "Invalid email or password"
+            render :new, status: :unprocessable_entity
+        end
+    end
+
 end
