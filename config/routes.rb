7e84559d6d967ee5bcee892cf9ddbbc475bd1a99
rakshit096 <rakshitroyalbrother@gmail.com>
2026-09-15 16:41:52 +0530

    feat: add login/logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   resources :articles
+
+  get "/login", to: "sessions#new"        #shows the login form
+  post "/login", to: "sessions#create"    #processes the login
+  delete "/logout", to: "sessions#destroy" #processes logout
+
 end
