40ad55ca9554b1483570efcb7795ba9f634dfab8
rakshit096 <rakshitroyalbrother@gmail.com>
2026-09-16 10:52:56 +0530

    feat: add delete article by owner only

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,12 @@ class ArticlesController < ApplicationController
       end
     end
 
+    def destroy
+    @article=current_user.articles.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+    end
+
 private
     def article_params
       params.require(:article).permit(:title, :description)
