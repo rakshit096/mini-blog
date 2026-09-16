bcb2af48a96cf23b22a83253cfa7403e34fa8f76
rakshit096 <rakshitroyalbrother@gmail.com>
2026-09-16 15:53:48 +0530

    chore add comment to helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?           # expose these methods to views
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
