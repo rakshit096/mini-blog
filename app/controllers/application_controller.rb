d1d392a9a1492a41b9d944d593eb218eb72864cb
rakshit096 <rakshitroyalbrother@gmail.com>
2026-09-16 10:39:38 +0530

    Feat: locked controllers and add create article controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,16 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
-  end   
+  end
 
   def logged_in?
     current_user.present?
-  end 
+  end
+
+  def require_user
+  unless logged_in?
+    flash[:alert] = "You must be logged in"
+    redirect_to login_path
+  end
+  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,25 @@
 class ArticlesController < ApplicationController
+    before_action :require_user, only: [ :new, :create, :edit, :update, :destroy ]  #to restrict controller actions
     def index
         @articles = Articles.all
+    end
+
+    def new
+      @article = Article.new
+    end
+
+    def create
+      @article = current_user.articles.build(article_params) ## Step 1: Takes form params AND automatically sets user_id = ex-5
+
+      if @article.save
+        redirect_to @article
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+private
+    def article_params
+      params.require(:article).permit(:title, :description)
     end
 end
